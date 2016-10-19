f86db2ca5de379748afd71e379ce2155f47d21e7
Tony DiCola <tony@tonydicola.com>
2016-10-19 01:48:28 +0000

    Fix python3 pip install ez_setup relative import.

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-from ez_setup import use_setuptools
-use_setuptools()
+try:
+    # Try using ez_setup to install setuptools if not already installed.
+    from ez_setup import use_setuptools
+    use_setuptools()
+except ImportError:
+    # Ignore import error and assume Python 3 which already has setuptools.
+    pass
+
 from setuptools import setup, find_packages
 
 classifiers = ['Development Status :: 4 - Beta',
@@ -12,7 +18,7 @@ classifiers = ['Development Status :: 4 - Beta',
                'Topic :: System :: Hardware']
 
 setup(name              = 'Adafruit_PCA9685',
-      version           = '1.0.0',
+      version           = '1.0.1',
       author            = 'Tony DiCola',
       author_email      = 'tdicola@adafruit.com',
       description       = 'Python code to use the PCA9685 PWM servo/LED controller with a Raspberry Pi or BeagleBone Black.',
