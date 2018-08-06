f4e43e612907960f785d75aa1e2df07702c23e7f
James Talbert <Talonj123@users.noreply.github.com>
2018-08-06 15:24:39 -0500

    Fixed an issue with frequency selections above ~12,456
    it used to go to a negative value, which meant it wrapped to the maximum prescaler, not the minimum.

--- Adafruit_PCA9685/PCA9685.py
+++ Adafruit_PCA9685/PCA9685.py
@@ -89,7 +89,7 @@ class PCA9685(object):
         prescaleval -= 1.0
         logger.debug('Setting PWM frequency to {0} Hz'.format(freq_hz))
         logger.debug('Estimated pre-scale: {0}'.format(prescaleval))
-        prescale = int(math.floor(prescaleval + 0.5))
+        prescale = int(math.min(0, math.floor(prescaleval + 0.5)))
         logger.debug('Final pre-scale: {0}'.format(prescale))
         oldmode = self._device.readU8(MODE1);
         newmode = (oldmode & 0x7F) | 0x10    # sleep
